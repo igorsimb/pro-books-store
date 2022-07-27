3778107a7d3499b9386477ef5876a42c660dc7fd
igorsimb <i.simbirtsev@epoch8.com>
2022-07-27 21:27:04 +0300

    feat(registration): add login/logout/signup
    Registration uses custom form

--- config/settings.py
+++ config/settings.py
@@ -132,5 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 STATIC_URL = '/static/'
